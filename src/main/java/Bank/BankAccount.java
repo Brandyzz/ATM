379ec19ef806fe8003b48b34ab175596d7d1be2f
Brandyzz <soyeremin@yandex.ru>
2020-08-04 14:23:59 +0300

    some change

--- src/main/java/Bank/BankAccount.java
+++ src/main/java/Bank/BankAccount.java
@@ -7,14 +7,10 @@ import java.util.Random;
 public final class BankAccount {
     private String pinCode;
     private final int bankAccountNumber;
-    private String firstName;
-    private String secondName;
     private int amount = 34567;
     private final Client client;
 
     public BankAccount(Client client, int bankAccountNumber){
-        this.firstName = client.getFirstName();
-        this.secondName = client.getSecondName();
         this.client = client;
         this.bankAccountNumber = bankAccountNumber;
     }
@@ -31,28 +27,12 @@ public final class BankAccount {
         return pinCode;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setSecondName(String secondName) {
-        this.secondName = secondName;
-    }
-
     public void setAmount(int amount) {
         this.amount = amount;
     }
 
     public int getBankAccountNumber() {
         return bankAccountNumber;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getSecondName() {
-        return secondName;
     }
 
     public int getAmount() {
